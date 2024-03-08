877a91eac5b1cfceae4200330ed82f7b2487e32c
Nikolay Nikolaev <nikolay.nikolaev25@gmail.com>
2024-03-08 08:57:14 +0200

    feat: add default button component

---
+++ template-parts/components/button.php
@@ -0,0 +1,17 @@
+<?php
+$url = ! empty( $args['url'] ) ? $args['url'] : '#';
+$text = ! empty( $args['text'] ) ? $args['text'] : 'Contact Us';
+$style = ! empty( $args['style'] ) ? $args['style'] : 'blue';
+$icon_name = ! empty( $args['icon'] ) ? $args['icon'] : false;
+?>
+
+<a href="<?php echo esc_url( $url ); ?>">
+    <button class="btn btn-<?php echo esc_html( $style ); ?>">
+        <?php esc_html_e( $text ); ?>
+        <?php if ( $icon_name ) : ?>
+        <span class="ml-2">
+            <?php \NyxitSoft\Helper::icon( $icon_name ); ?>
+        </span>
+        <?php endif; ?>
+    </button>
+</a>
