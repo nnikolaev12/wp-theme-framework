f2d28b67bc2a83672b6d5f8e498d6ee9cb06e26f
Nikolay Nikolaev <nikolay.nikolaev25@gmail.com>
2024-08-05 14:15:29 +0300

    feat: add fields support for create block script

--- scripts/create-block.php
+++ scripts/create-block.php
@@ -2,9 +2,12 @@
 #!/usr/bin/env php
 
 
-// Check if both arguments are provided
-if ($argc != 2) {
+// Check if required arguments are provided
+if ($argc < 2) {
     echo "Usage: php create-block.php [block name]\n";
+    echo "Usage with NPM: npm run create-block [block name]\n";
+    echo "Optional fields setting: npm run create-block [block name] --fields title:text\n";
+    echo "Check the themes README.md for more information.\n";
     exit(1);
 }
 
@@ -54,27 +57,118 @@ $blockTemplateFile = fopen( "$blockFolder/$blockName.php", "w" );
 if ( $blockTemplateFile ) {
     // Add contents to the block template file
     $blockTemplateContents = <<<EOT
-<?php
-/**
- * ACF Block: $capitalizedBlockName
- * 
- * @var array \$data
- */
+    <?php
+    /**
+     * ACF Block: $capitalizedBlockName
+     * 
+     * @var array \$data
+     */
 
-\$data = ! empty( \$args ) ? \$args : get_fields();
-\$block_classes = ! empty( \$block['className'] ) ? \$block['className'] : "";
-?>
-<section class="$blockName <?php echo \$block_classes; ?>">
+    \$data = ! empty( \$args ) ? \$args : get_fields();
+    \$block_classes = ! empty( \$block['className'] ) ? \$block['className'] : "";
+    ?>
+    <section class="$blockName <?php echo \$block_classes; ?>">
 
-</section>
-EOT;
+    </section>
+    EOT;
 
-fwrite($blockTemplateFile, $blockTemplateContents);
-fclose($blockTemplateFile);
-echo "$blockName.php file created successfully.\n";
-
+    fwrite($blockTemplateFile, $blockTemplateContents);
+    fclose($blockTemplateFile);
+    echo "$blockName.php file created successfully.\n";
 } else {
-echo "Error! Unable to create $blockName.php file.\n";
-exit(1);
+    echo "Error! Unable to create $blockName.php file.\n";
+    exit(1);
+}
+
+/**
+ * Create ACF json file with specified fields
+ * 
+ * TODO: Add support for repeater fields
+ */
+function format_fields( string $fields )
+{
+    $fieldsArray = explode( ",", $fields );
+    $formattedFields = "";
+
+    foreach ( $fieldsArray as $index => $field ) {
+        $fieldArray = explode( ":", $field );
+        $name = $fieldArray[0];
+        $type = $fieldArray[1];
+        $label = ucwords( str_replace( "_", " ", $name ) );
+        $hash = uniqid();
+        $comma = $index + 1 < count( $fieldsArray ) ? "," : "";
+
+        $formattedFields .= <<<EOT
+        {
+            "key": "field_$hash",
+            "label": "$label",
+            "name": "$name",
+            "aria-label": "",
+            "type": "$type",
+            "instructions": "",
+            "required": 0,
+            "conditional_logic": 0,
+            "wrapper": {
+                "width": "",
+                "class": "",
+                "id": ""
+            },
+            "default_value": "",
+            "maxlength": "",
+            "placeholder": "",
+            "prepend": "",
+            "append": ""
+        }$comma
+        EOT;
+    }
+
+    return $formattedFields;
+}
+
+if ( $argc === 3 ) {
+    $fields = format_fields( $argv[2] );
+
+    $timestamp = time();
+    $fileName = "group_block_$blockName" . "_" . uniqid();
+    $acfJsonFile = fopen( "./acf-json/$fileName.json", "w" );
+
+    if ( $acfJsonFile ) {
+        // Add contents to the ACF json file
+        $acfJsonContents = <<<EOT
+        {
+            "key": "$fileName",
+            "title": "[Block] $capitalizedBlockName",
+            "fields": [
+                $fields
+            ],
+            "location": [
+                [
+                    {
+                        "param": "block",
+                        "operator": "==",
+                        "value": "acf/$blockName"
+                    }
+                ]
+            ],
+            "menu_order": 0,
+            "position": "normal",
+            "style": "default",
+            "label_placement": "top",
+            "instruction_placement": "label",
+            "hide_on_screen": "",
+            "active": true,
+            "description": "",
+            "show_in_rest": 0,
+            "modified": $timestamp
+        }
+        EOT;
+
+        fwrite($acfJsonFile, $acfJsonContents);
+        fclose($acfJsonFile);
+        echo "acf-$fileName.json file created successfully.\n";
+    } else {
+        echo "Error! Unable to create acf-$fileName.json file.\n";
+        exit(1);
+    }
 }
 ?>
